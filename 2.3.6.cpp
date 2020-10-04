23fc1ddb860a89d435782ae06c8584aa2e560631
larben30 <72338468+larben30@users.noreply.github.com>
2020-10-04 15:00:26 +0500

    Add files via upload

---
+++ 2.3.6.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <cstdlib>
+#include <ctime>
+#include <array>
+
+using namespace std;
+
+class BoundedRandom {
+	int low;
+	int high;
+public:
+	BoundedRandom(int l, int h) : low(l), high(h) {}
+	int operator () () { return rand() % (high - low + 1) + low; }
+};
+
+int main() {
+	int* arr;
+	size_t size;
+	std::cout << "Array size: ";
+	std::cin >> size;
+
+	arr = new int[size];
+	srand(time(NULL));
+	std::generate_n(arr, size, BoundedRandom(0, 100));
+
+	std::cout << "Array:" << std::endl;
+	std::copy(arr, arr + size, std::ostream_iterator<int>(std::cout, " "));
+	
+	delete[] arr;
+	return 0;
+}
