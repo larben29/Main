5b9aa24ff749a68c7902dd2822201772e17f51b3
larben30 <72338468+larben30@users.noreply.github.com>
2020-10-04 14:59:21 +0500

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int number;
+    cout << "enter integer number: ";
+    cin >> number;
+    cout << "Number A: " << number  << "\n";
+    int number2;
+    cout << "enter integer number: ";
+    cin >> number2;
+    cout << "Number B: " << number2 << "\n";
+    system("pause");
+    return 0;
+}
